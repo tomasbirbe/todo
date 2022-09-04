6f251978a7d67dace48f2c08b5d00448a7e22cbb
Tomas <tomas.birbe@gmail.comm>
2022-09-04 14:02:16 -0300

    main: add uuid and task can be done or not be done

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
-import React, { useState } from "react";
+import React, { ChangeEvent, useState } from "react";
+import { v4 } from "uuid";
+
+interface Task {
+  content: string;
+  id: string;
+  isDone: boolean;
+}
 
 function App() {
   const [newTask, setNewTask] = useState<string>("");
-  const [tasks, setTasks] = useState<string[]>([]);
+  const [tasks, setTasks] = useState<Task[]>([]);
 
   function handleSubmit(event: React.FormEvent) {
     event.preventDefault();
 
-    setTasks((prevState) => [...prevState, newTask]);
+    setTasks((prevState) => [...prevState, { content: newTask, id: v4(), isDone: false }]);
     setNewTask("");
   }
 
@@ -15,22 +22,41 @@ function App() {
     setNewTask(event.target.value);
   }
 
+  function handleCheck(task: Task) {
+    const updatedTask = tasks.map((a: Task) => {
+      if (task.id === a.id) {
+        a.isDone = !a.isDone;
+      }
+
+      return a;
+    });
+
+    setTasks(updatedTask);
+  }
+
   return (
-    <div className="App grid place-items-center h-full">
-      <div>
-        <ul>
-          {tasks.map((task) => (
-            <li key={Math.random() * 100000}>{task}</li>
-          ))}
-        </ul>
-        <form onSubmit={handleSubmit}>
+    <div className="App grid place-items-center">
+      <div className="pt-4 flex flex-col gap-4">
+        <form className="flex gap-5" onSubmit={handleSubmit}>
           <input
-            className="border border-black"
+            className="border border-black p-2 rounded-md"
+            placeholder="Creá una nueva tarea"
             type="text"
             value={newTask}
             onChange={handleInput}
           />
+          <button className="border border-black px-2 rounded-md" type="submit">
+            Crear
+          </button>
         </form>
+        <ul>
+          {tasks.map((task) => (
+            <div key={task.id} className="flex items-center justify-between px-2">
+              <li className={`${task.isDone ? "line-through" : ""}`}>{task.content}</li>
+              <input type="checkbox" onChange={() => handleCheck(task)} />
+            </div>
+          ))}
+        </ul>
       </div>
     </div>
   );
