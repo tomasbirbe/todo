e751025eecf662f9af3acb9f4906b7aed151395f
Tomas <tomas.birbe@gmail.comm>
2022-09-04 01:15:59 -0300

    main: add an input and a basic list

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,37 @@
-import { useState } from "react";
+import React, { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [newTask, setNewTask] = useState<string>("");
+  const [tasks, setTasks] = useState<string[]>([]);
+
+  function handleSubmit(event: React.FormEvent) {
+    event.preventDefault();
+
+    setTasks((prevState) => [...prevState, newTask]);
+    setNewTask("");
+  }
+
+  function handleInput(event: React.ChangeEvent<HTMLInputElement>) {
+    setNewTask(event.target.value);
+  }
 
   return (
-    <div className="App">
-      <p>Esto es una prueba</p>
+    <div className="App grid place-items-center h-full">
+      <div>
+        <ul>
+          {tasks.map((task) => (
+            <li key={Math.random() * 100000}>{task}</li>
+          ))}
+        </ul>
+        <form onSubmit={handleSubmit}>
+          <input
+            className="border border-black"
+            type="text"
+            value={newTask}
+            onChange={handleInput}
+          />
+        </form>
+      </div>
     </div>
   );
 }
